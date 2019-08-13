732f447cfd894d98bec8b70f629c0d443ff0cd6d
Parashuram N <axemclion@users.noreply.github.com>
2019-08-12 21:16:00 -0700

    Add ReactMarkers to measure indivudual segments

--- app/src/main/java/ctrip/wileless/android/crn/demo/BaseApplication.java
+++ app/src/main/java/ctrip/wileless/android/crn/demo/BaseApplication.java
@@ -7,19 +7,34 @@
 package ctrip.wileless.android.crn.demo;
 
 import android.app.Application;
+import android.util.Log;
 
+
+import com.facebook.react.bridge.ReactMarker;
+import com.facebook.react.bridge.ReactMarkerConstants;
+
+import javax.annotation.Nullable;
 
 import ctrip.wireless.android.crn.ContextHolder;
 
 public class BaseApplication extends Application {
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        ContextHolder.context = this;
-        ContextHolder.version = "1.0.0";
-        ContextHolder.debug = true;
-        ContextHolder.application = this;
-    }
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    ContextHolder.context = this;
+    ContextHolder.version = "1.0.0";
+    ContextHolder.debug = true;
+    ContextHolder.application = this;
+
+    final long startTime = System.currentTimeMillis();
+
+    ReactMarker.addListener(new ReactMarker.MarkerListener() {
+      @Override
+      public void logMarker(ReactMarkerConstants name, @Nullable String tag, int instanceKey) {
+        Log.i("AXE", name.toString() + " " + tag + " " + instanceKey + ": " + (System.currentTimeMillis() - startTime));
+      }
+    });
+  }
 
 }
